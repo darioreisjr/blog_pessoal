90cd5b738deb1800385e025bb1bc573049666a4b
Dario Reis <guttyerresjunior1995@gmail.com>
2022-06-30 14:40:05 -0300

    adiciona atualização do banco de dados do blog pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,21 +1,23 @@
 package com.generation.blogpessoal.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
+// tranforma o objeto de postagem em uma tabela no banco de dados
 @Entity
 @Table(name = "postagem")
 public class Postagem {
 	
+	//define a chave primaria da tabela 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -28,9 +30,9 @@ public class Postagem {
 	@Size(min = 10, max = 500)
 	private String texto;
 	
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date date = new java.sql.Date(System.currentTimeMillis()) ;
+	// Anotação que pega automaticamente hora e data do seu computador
+	@UpdateTimestamp 
+	private LocalDateTime data;
 
 	public long getId() {
 		return id;
@@ -56,14 +58,13 @@ public class Postagem {
 		this.texto = texto;
 	}
 
-	public Date getDate() {
-		return date;
+	public LocalDateTime getData() {
+		return data;
 	}
 
-	public void setDate(Date date) {
-		this.date = date;
+	public void setData(LocalDateTime data) {
+		this.data = data;
 	}
-	
-	
+
 
 }
