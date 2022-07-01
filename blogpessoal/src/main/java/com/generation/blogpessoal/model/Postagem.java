a666e8fcdfd4bc2431047bd2022b0cf3de3f9794
Dario Reis <guttyerresjunior1995@gmail.com>
2022-07-01 14:04:30 -0300

    adiciona atualização as tabelas do banco de dados e adiciona tabela Tema

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,33 +6,40 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 // tranforma o objeto de postagem em uma tabela no banco de dados
 @Entity
 @Table(name = "postagem")
 public class Postagem {
-	
-	//define a chave primaria da tabela 
+
+	// define a chave primaria da tabela
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
+
 	// Anotação que pega automaticamente hora e data do seu computador
-	@UpdateTimestamp 
+	@UpdateTimestamp
 	private LocalDateTime data;
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -66,5 +73,12 @@ public class Postagem {
 		this.data = data;
 	}
 
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
